2213e4b0d6ae7bd54ba72c058047c0137b9669ea
Sekar Nagarajan <sekarnagarajan@gmail.com>
2024-05-14 21:46:08 +0530

    jsx

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Github from './components/Github/Github.jsx'
 import Home from './components/Home/Home.jsx'
+import User from './components/User/User.jsx'
 import './index.css'
 import Layout from './Layout.jsx'
 
@@ -11,6 +14,9 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
+      <Route path="user" element={<User />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="github" element={<Github />} />
     </Route>
   )
 )
