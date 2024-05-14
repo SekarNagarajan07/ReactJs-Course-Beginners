de042bcbc6fa12db3380bdbc0140907cdff0c907
Sekar Nagarajan <sekarnagarajan@gmail.com>
2024-05-14 22:08:54 +0530

    jsx

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 import Home from './components/Home/Home.jsx'
 import User from './components/User/User.jsx'
 import './index.css'
@@ -14,9 +14,12 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
-      <Route path="user" element={<User />} />
+      <Route path='user/' element={<User />} >
+        <Route path=':userid' element={<User />} />
+      </Route>
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="github" element={<Github />} />
+      <Route path='*' element={<div>Not Found</div>} />
     </Route>
   )
 )
