301cdb99456b6f400bc46afa8072dab3fbcc3dac
Sekar Nagarajan <sekarnagarajan@gmail.com>
2024-05-18 11:40:33 +0530

    jsx

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import { TodoProvider } from './contexts/'
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo
+      )
+    )
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("Todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("Todos", JSON.stringify(todos))
+  }, [todos])
   return (
-    <>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
 
-    </>
+    </TodoProvider>
   )
 }
 
